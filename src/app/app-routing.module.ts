a6062521ac89385166c07b28ddc87cc8128e04e2
Kunal <kunalmadne10@gmail.com>
2020-01-31 11:28:25 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListuserComponent } from './components/listuser/listuser.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:ListuserComponent},
+  {path:"op",component:UserFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
